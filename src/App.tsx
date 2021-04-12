4c9b7e7c1e78af177c1e63485a905f741ecef065
Anton <antonzhivaev11@gmail.com>
2021-04-12 15:11:22 +0300

    sixth commit fix:         - unnecessary use of contexts removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,20 @@ import { FC } from "react";
 import "./App.css";
 import Logo from "./components/Logo";
 import TodoMainComponent from "./components/todo/TodoMainComponent";
-import { TodosProvider } from "./store/TodosContext";
-import { FilterActivityProvider } from "./store/FilterTodosActivityContext";
 import TodosAppProvider from "./store/TodosAppProvider";
 
 const App: FC = () => {
   return (
-    <TodosProvider>
-      <TodosAppProvider>
-        <div className="App">
-          <header>
-            <Logo />
-          </header>
-          <section>
-            <FilterActivityProvider>
-              <TodoMainComponent />
-            </FilterActivityProvider>
-          </section>
-        </div>
-      </TodosAppProvider>
-    </TodosProvider>
+    <TodosAppProvider>
+      <div className="App">
+        <header>
+          <Logo />
+        </header>
+        <section>
+          <TodoMainComponent />
+        </section>
+      </div>
+    </TodosAppProvider>
   );
 };
 
